5a07e180aebbc5fdfb7e0ac9911109f151b2f44b
Erison88 <songerison.mse08@nctu.edu.tw>
2023-03-20 00:42:07 +0800

    add comment

--- hw3/heapsort.cpp
+++ hw3/heapsort.cpp
@@ -52,12 +52,12 @@ void Build_Max_Heap(int *arr, int n){
 void Heapsort(int *arr, int n){
     Build_Max_Heap(arr, n);
     // one by one extract an element from heap
-    /*
+    
     for (int i=n-1; i>=1; i--){
         swap(arr[0], arr[i]);   // move current root to end
         MaxHeapify(arr, i, 0);  // call max heapify on the reduced heap
     }
-    */
+    
 }
 /*
 將Max-heap的root與最後一個節點交換，並排除最後一個節點，
