20a3feffa849975413291052c2deedc46412c31e
Erison88 <songerison.mse08@nctu.edu.tw>
2023-03-19 22:37:05 +0800

    add comment

--- hw3/heapsort.cpp
+++ hw3/heapsort.cpp
@@ -24,24 +24,28 @@ int main(int argc, char ** argv){
 
 void MaxHeapify(int *arr, int n, int i){
     int largest = i;
-    int left = 2*i+1;
+    int left = 2*i+1;   
     int right = 2*i+2;
 
+    // if left child is larger than root
     if(left<n && arr[left]>arr[largest]){
         largest = left;
     }
 
+    // if right child is larger than largest so far
     if (right<n && arr[right]>arr[largest]) {
         largest = right;
     }
 
+    // if largest is not root
     if (largest != i) {
         swap(arr[i], arr[largest]);
-        MaxHeapify(arr, n, largest);
+        MaxHeapify(arr, n, largest);    // recursively heapify the affected sub-tree
     }
 }
 
 void Build_Max_Heap(int *arr, int n){
+    // build heap(rearrange array)
     for(int i=n/2-1; i>=0; i--){
         MaxHeapify(arr, n, i);
     }
@@ -49,8 +53,9 @@ void Build_Max_Heap(int *arr, int n){
 
 void Heapsort(int *arr, int n){
     Build_Max_Heap(arr, n);
+    // one by one extract an element from heap
     for (int i=n-1; i>=1; i--){
-        swap(arr[0], arr[i]);
-        MaxHeapify(arr, i, 0);
+        swap(arr[0], arr[i]);   // move current root to end
+        MaxHeapify(arr, i, 0);  // call max heapify on the reduced heap
     }
 }
