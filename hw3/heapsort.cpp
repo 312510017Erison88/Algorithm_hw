2fba89f0135840296416b761ca7783988960cfee
Erison88 <songerison.mse08@nctu.edu.tw>
2023-03-21 17:30:59 +0800

    user can input any size

--- hw3/heapsort.cpp
+++ hw3/heapsort.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,6 +10,31 @@ void Build_Max_Heap(int *arr, int n);
 void Heapsort(int *arr, int n);
 
 int main(int argc, char ** argv){
+    double START, END;
+    int size;
+    cout << "Input an integer for an array size, and press crtl+d to exit:";
+    while(cin >> size){
+        int *a = new int[size];
+        for(int i=0; i<size; i++){
+            a[i] = rand()%size;
+        }
+        START = clock();
+        Heapsort(a, size);
+        END = clock();
+        cout << "整個程式執行時間:" << (double)clock()/CLOCKS_PER_SEC << " S" << endl;
+        cout << "heapsort fuction進行運算的執行時間:" << (END-START) / CLOCKS_PER_SEC << " S" << endl;
+        delete [] a;
+        a = NULL;
+        //print a[]
+        /*
+        for(int i=0; i<size; i++){
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        */
+    }
+    /////////////////////////////////////////
+    /*
     int a[7] = {1,2,5,3,4,7,6};
 
     Heapsort(a, 7);
@@ -16,7 +43,7 @@ int main(int argc, char ** argv){
         cout << a[i] << " ";
     }
     cout << endl;
-
+*/
     cout << "this is the the last line!" << endl;
 
     return 0;
@@ -52,12 +79,10 @@ void Build_Max_Heap(int *arr, int n){
 void Heapsort(int *arr, int n){
     Build_Max_Heap(arr, n);
     // one by one extract an element from heap
-    
     for (int i=n-1; i>=1; i--){
         swap(arr[0], arr[i]);   // move current root to end
         MaxHeapify(arr, i, 0);  // call max heapify on the reduced heap
     }
-    
 }
 /*
 將Max-heap的root與最後一個節點交換，並排除最後一個節點，
