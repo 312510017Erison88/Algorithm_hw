21d85da78f81ffd8505d3495014503943f8bcf8a
Erison88 <songerison.mse08@nctu.edu.tw>
2023-05-09 18:25:52 +0800

    add cut_position and min in bottom up

--- hw5/Dynamic_programming.cpp
+++ hw5/Dynamic_programming.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+struct Bottom_up{
+    int max_price;
+    int min_price;
+    vector <int> max_cut_posi;
+    vector <int> min_cut_posi;
+};
 
 int Cut_Rod(int* price, int n);
 int Memorized_Cut_Rod(int* price, int n);
 int Memorized_Cut_Rod_Aux(int* price, int n, int* table);
-int Bottom_up_Cut_Rod(int* price, int n);
+Bottom_up Bottom_up_Cut_Rod(int* price, int n);
+
 // goal: write the top-down and bottom-up method to get max and min price as well as the way you cut
 int main(){
     int price[] = {1,5,8,9,10,17,17,20,24,30};
 
     int maxprice = Cut_Rod(price, 4);
     int maxprice_2 = Memorized_Cut_Rod(price, 4);
-    int maxprice_3 = Bottom_up_Cut_Rod(price, 4);
+    Bottom_up maxprice_3 = Bottom_up_Cut_Rod(price, 4);
     cout << maxprice << endl;
     cout << maxprice_2 << endl;
-    cout << maxprice_3 << endl;
+    cout << "total length: " << "4" << endl;
+    cout << "maximum price: " << maxprice_3.max_price << endl;
+    cout << "切法: ";
+    for (int i = 0; i < maxprice_3.max_cut_posi.size(); i++){
+        cout << maxprice_3.max_cut_posi[i] << " ";
+    }
+    cout << endl;
+    cout << "number of pieces: " << maxprice_3.max_cut_posi.size() << endl;
+    cout << endl;
+    //////////////////////////////////////////////////////
+    cout << "minimum price: " << maxprice_3.min_price << endl;
+    cout << "切法: ";
+    for (int i = 0; i < maxprice_3.min_cut_posi.size(); i++){
+        cout << maxprice_3.min_cut_posi[i] << " ";
+    }
+    cout << endl;
+    cout << "number of pieces: " << maxprice_3.min_cut_posi.size() << endl;
     
     return 0;
 }
@@ -58,17 +83,43 @@ int Memorized_Cut_Rod_Aux(int* price, int n, int* table){
     }
 }
 
-int Bottom_up_Cut_Rod(int* price, int n){
-    int* table = new int[n+1];
-    table[0] = 0;
+Bottom_up Bottom_up_Cut_Rod(int* price, int n){
+    Bottom_up box;
+    int* max_table = new int[n+1];
+    int* min_table = new int[n+1];
+    vector <int> max_cut_position;
+    vector <int> min_cut_position;
+    max_table[0] = 0;
+    min_table[0] = 0;
+
     for(int i=1; i<=n; i++){
-        int temp = -100000;
+        int max_price = -100000;
+        int min_price = 100000;
+        int max_cutIndex = 0;
+        int min_cutIndex = 0;
         for(int j=0; j<i; j++){
-            temp = max(temp, price[j] + table[i-j-1]);
+            int current_price = price[j] + max_table[i-j-1];
+            if(current_price > max_price){
+                max_price = current_price;
+                max_cutIndex = j;
+            }
+            if(current_price < min_price){
+                min_price = current_price;
+                min_cutIndex = j;
+            }
         }
-        table[i] = temp;
+        max_table[i] = max_price;
+        min_table[i] = min_price; // wierd for this line!!!!!!!!
+        max_cut_position.push_back(max_cutIndex+1);
+        min_cut_position.push_back(min_cutIndex+1);
     }
-    int maxPrice = table[n];
-    delete[] table;
-    return maxPrice;
+    
+    box.max_price = max_table[n];
+    box.min_price = min_table[n];
+    box.max_cut_posi = max_cut_position;
+    box.min_cut_posi = min_cut_position;
+    delete[] max_table;
+    delete[] min_table;
+
+    return box;
 }
