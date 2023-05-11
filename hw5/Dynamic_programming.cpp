d6cb8f12341c170132abbc4b6f744d6f4d61521e
Erison88 <songerison.mse08@nctu.edu.tw>
2023-05-11 15:10:05 +0800

    min price is correct

--- hw5/Dynamic_programming.cpp
+++ hw5/Dynamic_programming.cpp
@@ -19,40 +19,40 @@ Bottom_up Bottom_up_Cut_Rod(int* price, int n);
 int main(){
     int price[] = {1,5,8,9,10,17,17,20,24,30};
 
-    int maxprice = Cut_Rod(price, 4);
-    int maxprice_2 = Memorized_Cut_Rod(price, 4);
-    Bottom_up maxprice_3 = Bottom_up_Cut_Rod(price, 4);
+    int maxprice = Cut_Rod(price, 8);
+    int maxprice_2 = Memorized_Cut_Rod(price, 8);
+    Bottom_up price_3 = Bottom_up_Cut_Rod(price, 8);
     cout << maxprice << endl;
     cout << maxprice_2 << endl;
     cout << "total length: " << "4" << endl;
-    cout << "maximum price: " << maxprice_3.max_price << endl;
+    cout << "maximum price: " << price_3.max_price << endl;
     cout << "切法: ";
-    for (int i = 0; i < maxprice_3.max_cut_posi.size(); i++){
-        cout << maxprice_3.max_cut_posi[i] << " ";
+    for (int i = 0; i < price_3.max_cut_posi.size(); i++){
+        cout << price_3.max_cut_posi[i] << " ";
     }
     cout << endl;
-    cout << "number of pieces: " << maxprice_3.max_cut_posi.size() << endl;
+    cout << "number of pieces: " << price_3.max_cut_posi.size() << endl;
     cout << endl;
     //////////////////////////////////////////////////////
-    cout << "minimum price: " << maxprice_3.min_price << endl;
+    cout << "minimum price: " << price_3.min_price << endl;
     cout << "切法: ";
-    for (int i = 0; i < maxprice_3.min_cut_posi.size(); i++){
-        cout << maxprice_3.min_cut_posi[i] << " ";
+    for (int i = 0; i < price_3.min_cut_posi.size(); i++){
+        cout << price_3.min_cut_posi[i] << " ";
     }
     cout << endl;
-    cout << "number of pieces: " << maxprice_3.min_cut_posi.size() << endl;
+    cout << "number of pieces: " << price_3.min_cut_posi.size() << endl;
     
     return 0;
 }
 
 int Cut_Rod(int* price, int n){
-    int q = -100000000;
+    int q = 100000000;
     if(n==0){       // size=0, then terminate
         return 0;
     }
     else{
         for(int i=0; i<n; i++){
-            q = max(q, price[i] + Cut_Rod(price, n-i-1));
+            q = min(q, price[i] + Cut_Rod(price, n-i-1));
         }
     }
     return q;
@@ -92,19 +92,20 @@ Bottom_up Bottom_up_Cut_Rod(int* price, int n){
     max_table[0] = 0;
     min_table[0] = 0;
 
-    for(int i=1; i<=n; i++){
+    for(int i=1; i<=n; i++){            // record the table
         int max_price = -100000;
         int min_price = 100000;
         int max_cutIndex = 0;
         int min_cutIndex = 0;
         for(int j=0; j<i; j++){
-            int current_price = price[j] + max_table[i-j-1];
-            if(current_price > max_price){
-                max_price = current_price;
+            int max_current_price = price[j] + max_table[i-j-1];
+            int min_current_price = price[j] + min_table[i-j-1];
+            if(max_current_price > max_price){
+                max_price = max_current_price;
                 max_cutIndex = j;
             }
-            if(current_price < min_price){
-                min_price = current_price;
+            if(min_current_price < min_price){
+                min_price = min_current_price;
                 min_cutIndex = j;
             }
         }
