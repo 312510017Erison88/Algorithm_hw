314dfba475cf408efa52403299ecab52e4cdbc5b
Erison88 <songerison.mse08@nctu.edu.tw>
2023-05-14 22:03:50 +0800

    finish two common sequence comparison

--- hw6/longest_common_sequence.cpp
+++ hw6/longest_common_sequence.cpp
@@ -3,75 +3,76 @@
 
 using namespace std;
 
-struct Table{
-    vector<int> arrow;
-    vector<int> length;
-};
+void longest_common_seq(double* X, double* Y, int** length, int** direction, int size_X, int size_Y);
+void print_LCS(double* X, int** length, int** direction, int i, int j);
 
-Table longest_common_seq(double X[], double Y[], int size_X, int size_Y);
-void print_LCS(const Table& table, double X[], int i, int j);
 int main(){
-    double X[] = {1.0, 2.0, 3.0, 2.0, 4.0, 1.0, 2.0};
-    double Y[] = {2.0, 4.0, 3.0, 1.0, 2.0, 1.0};
+    double X[] = {1.0, 4.0, 6.0, 2.0, 8.0, 1.0, 5.0, 6.0};
+    double Y[] = {1.0, 6.0, 9.0, 8.0, 5.0, 1.0, 6, 3, 2};
     int size_X = sizeof(X) / sizeof(X[0]);
     int size_Y = sizeof(Y) / sizeof(Y[0]);
-    cout << size_X << " " << size_Y << endl;
 
-    Table result = longest_common_seq(X, Y, size_X, size_Y);
+    int** length = new int*[size_X + 1];
+    int** direction = new int*[size_X + 1];
+    for(int i=0; i<size_X+1; i++){
+        length[i] = new int[size_Y + 1];
+        direction[i] = new int[size_Y + 1];
+    }
 
-    cout << "Longest Common Sequence: ";
-    print_LCS(result, X, size_X, size_Y);
-    cout << endl;
+    longest_common_seq(X, Y, length, direction, size_X, size_Y);
+
+    for(int i=0; i < size_X+1; i++){
+        delete[] length[i];
+        delete[] direction[i];
+    }
+    delete[] length;
+    delete[] direction;
 
     return 0;
 }
 
-Table longest_common_seq(double X[], double Y[], int size_X, int size_Y){
-    Table table;
-    table.arrow.resize((size_X + 1) * (size_Y + 1));
-    table.length.resize((size_X) * (size_Y)); // wierd for this line!
-
-    // Initialize the first row and column of C to 0
-    for(int i=0; i<size_X; i++){
-        table.length[i] = 0;
+void longest_common_seq(double* X, double* Y, int** length, int** direction, int size_X, int size_Y){
+    // Initialize the length and direction arrays
+    for(int i=0; i<size_X+1; i++){
+        length[i][0] = 0;
     }
-    for(int j=0; j<size_Y; j++){
-        table.length[j * (size_X)] = 0;
+    for(int i=0; i<size_Y+1; i++){
+        length[0][i] = 0;
     }
-    cout << "good" << endl;
 
     // Compute the LCS table
-    for(int i = 0; i < size_X; i++){
-        for(int j = 0; j < size_Y; j++){
-            if(X[i] == Y[j]){
-                table.length[j * (size_X)+i] = table.length[(j-1) * (size_X) + (i-1)] + 1;
-                table.arrow[j * (size_X+1)+i] = 0;  // "up-left arrow"
+    for(int i=1; i<size_X+1; i++){
+        for(int j=1; j<size_Y+1; j++){
+            if(X[i-1] == Y[j-1]){
+                length[i][j] = length[i-1][j-1] + 1;
+                direction[i][j] = 0;  // "up-left arrow"
             }
-            else if(table.length[(j) * (size_X) + (i-1)] >= table.length[(j-1) * (size_X) + i]){
-                table.length[j * (size_X) + i] = table.length[j * (size_X) + (i-1)];
-                table.arrow[j * (size_X+1) + i] = 1;  // "up arrow"
+            else if(length[i-1][j] >= length[i][j-1]){
+                length[i][j] = length[i-1][j];
+                direction[i][j] = 1;  // "up arrow"
             }
             else{
-                table.length[j * (size_X) + i] = table.length[j * (size_X) + (i-1)];
-                table.arrow[j * (size_X+1) + i] = 2;  // "left arrow"
+                length[i][j] = length[i][j-1];
+                direction[i][j] = 2;  // "left arrow"
             }
         }
     }
-    return table;
+    print_LCS(X, length, direction, size_X, size_Y);
+    cout << endl;
 }
 
-void print_LCS(const Table& table, double X[], int i, int j){
+void print_LCS(double* X, int** length, int** direction, int i, int j){
     if(i == 0 || j == 0){
         return;
     }
-    if(table.arrow[j * (i+1) + i] == 0){
-        print_LCS(table, X, i-1, j-1);
+    if(direction[i][j] == 0){
+        print_LCS(X, length, direction, i-1, j-1);
         cout << X[i-1] << " ";
     }
-    else if(table.arrow[j * (i+1) + i] == 1){
-        print_LCS(table, X, i-1, j);
+    else if(direction[i][j] == 1){
+        print_LCS(X, length, direction, i-1, j);
     }
     else{
-        print_LCS(table, X, i, j-1);
+        print_LCS(X, length, direction, i, j-1);
     }
 }
