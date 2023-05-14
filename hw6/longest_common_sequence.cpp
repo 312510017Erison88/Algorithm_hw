ddc5c20706a709da515152e8feb77cc5ad15d095
Erison88 <songerison.mse08@nctu.edu.tw>
2023-05-14 16:57:37 +0800

    something wrong

--- hw6/longest_common_sequence.cpp
+++ hw6/longest_common_sequence.cpp
@@ -29,29 +29,30 @@ int main(){
 Table longest_common_seq(double X[], double Y[], int size_X, int size_Y){
     Table table;
     table.arrow.resize((size_X + 1) * (size_Y + 1));
-    table.length.resize((size_X + 1) * (size_Y + 1)); // wierd for this line!
+    table.length.resize((size_X) * (size_Y)); // wierd for this line!
 
     // Initialize the first row and column of C to 0
-    for(int i=0; i<=size_X; i++){
+    for(int i=0; i<size_X; i++){
         table.length[i] = 0;
     }
-    for(int j=0; j<=size_Y; j++){
-        table.length[j * (size_X + 1)] = 0;
+    for(int j=0; j<size_Y; j++){
+        table.length[j * (size_X)] = 0;
     }
+    cout << "good" << endl;
 
     // Compute the LCS table
-    for(int i = 1; i <= size_X; i++){
-        for(int j = 1; j <= size_Y; j++){
-            if(X[i-1] == Y[j-1]){
-                table.length[j * (size_X+1)+i] >= table.length[(j-1) * (size_X+1) + i];
+    for(int i = 0; i < size_X; i++){
+        for(int j = 0; j < size_Y; j++){
+            if(X[i] == Y[j]){
+                table.length[j * (size_X)+i] = table.length[(j-1) * (size_X) + (i-1)] + 1;
                 table.arrow[j * (size_X+1)+i] = 0;  // "up-left arrow"
             }
-            else if(table.length[(j-1) * (size_X+1) + i] >= table.length[j * (size_X+1) + (i-1)]){
-                table.length[j * (size_X+1) + i] = table.length[(j-1) * (size_X+1) + i];
+            else if(table.length[(j) * (size_X) + (i-1)] >= table.length[(j-1) * (size_X) + i]){
+                table.length[j * (size_X) + i] = table.length[j * (size_X) + (i-1)];
                 table.arrow[j * (size_X+1) + i] = 1;  // "up arrow"
             }
             else{
-                table.length[j * (size_X+1) + i] = table.length[j * (size_X+1) + (i-1)];
+                table.length[j * (size_X) + i] = table.length[j * (size_X) + (i-1)];
                 table.arrow[j * (size_X+1) + i] = 2;  // "left arrow"
             }
         }
@@ -63,14 +64,14 @@ void print_LCS(const Table& table, double X[], int i, int j){
     if(i == 0 || j == 0){
         return;
     }
-    if(table.arrow[j * (table.length.size() / (i+1)) + j] == 0){
+    if(table.arrow[j * (i+1) + i] == 0){
         print_LCS(table, X, i-1, j-1);
         cout << X[i-1] << " ";
     }
-    else if(table.arrow[j * (table.length.size() / (j+1)) + i] == 1){
-        print_LCS(table, X, i, j-1);
+    else if(table.arrow[j * (i+1) + i] == 1){
+        print_LCS(table, X, i-1, j);
     }
     else{
-        print_LCS(table, X, i-1, j);
+        print_LCS(table, X, i, j-1);
     }
 }
