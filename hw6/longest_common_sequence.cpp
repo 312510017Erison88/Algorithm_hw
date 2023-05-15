14d68278202df8931e1c683ae6521e79bd83f95b
Erison88 <songerison.mse08@nctu.edu.tw>
2023-05-15 17:29:25 +0800

    result dynamic allocate have problem

--- hw6/longest_common_sequence.cpp
+++ hw6/longest_common_sequence.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 void longest_common_seq(double* X, double* Y, int** length, int** direction, int size_X, int size_Y);
-void print_LCS(double* X, int** length, int** direction, int i, int j);
+void print_LCS(double* X, int** length, int** direction, int i, int j, double* result);
 
 int main(){
     double X[] = {1.0, 4.0, 6.0, 2.0, 8.0, 1.0, 5.0, 6.0};
     double Y[] = {1.0, 6.0, 9.0, 8.0, 5.0, 1.0, 6, 3, 2};
+    double Z[] = {1, 5, 6, 8, 1, 22};
     int size_X = sizeof(X) / sizeof(X[0]);
     int size_Y = sizeof(Y) / sizeof(Y[0]);
+    int size_Z = sizeof(Z) / sizeof(Z[0]);
 
     int** length = new int*[size_X + 1];
     int** direction = new int*[size_X + 1];
@@ -18,7 +20,26 @@ int main(){
         direction[i] = new int[size_Y + 1];
     }
 
+    int** length_2 = new int*[size_X + 1];
+    int** direction_2 = new int*[size_X + 1];
+    for(int i=0; i<size_X+1; i++){
+        length_2[i] = new int[size_Z + 1];
+        direction_2[i] = new int[size_Z + 1];
+    }
+
+    double* result1 = new double[size_Y];       //wierd!!!!!!!!!
+    double* result2 = new double[size_Y];
+    double* final_result = new double[size_X];
+
     longest_common_seq(X, Y, length, direction, size_X, size_Y);
+    print_LCS(X, length, direction, size_X, size_Y, result1);
+    cout << endl;
+    longest_common_seq(X, Z, length_2, direction_2, size_X, size_Z);
+    print_LCS(X, length_2, direction_2, size_X, size_Z, result2);
+    cout << endl;
+    longest_common_seq(result1, result2, length_2, direction_2, size_X, size_Z);
+    print_LCS(result1, length_2, direction_2, size_X, size_Y, final_result);
+    cout << endl;
 
     // Free the dynamically allocated memory
     for(int i=0; i < size_X+1; i++){
@@ -27,6 +48,16 @@ int main(){
     }
     delete[] length;
     delete[] direction;
+
+    for(int i=0; i < size_X+1; i++){
+        delete[] length_2[i];
+        delete[] direction_2[i];
+    }
+    delete[] length_2;
+    delete[] direction_2;
+    delete[] result1;
+    delete[] result2;
+    delete[] final_result;
 
     return 0;
 }
@@ -58,23 +89,24 @@ void longest_common_seq(double* X, double* Y, int** length, int** direction, int
             }
         }
     }
-    print_LCS(X, length, direction, size_X, size_Y);
-    cout << endl;
+    //print_LCS(X, length, direction, size_X, size_Y);
+    //cout << endl;
 }
 
 // Print the result
-void print_LCS(double* X, int** length, int** direction, int i, int j){
+void print_LCS(double* X, int** length, int** direction, int i, int j, double* result){
     if(i == 0 || j == 0){
         return;
     }
     if(direction[i][j] == 0){
-        print_LCS(X, length, direction, i-1, j-1);
+        print_LCS(X, length, direction, i-1, j-1, result);
         cout << X[i-1] << " ";
+        result[i-1] = X[i-1];   //wierd!
     }
     else if(direction[i][j] == 1){
-        print_LCS(X, length, direction, i-1, j);
+        print_LCS(X, length, direction, i-1, j, result);
     }
     else{
-        print_LCS(X, length, direction, i, j-1);
+        print_LCS(X, length, direction, i, j-1, result);
     }
 }
